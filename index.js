2ccf01e3cd4c5cba66e320010b611507075b1e3b
Aaron Wilson <aaron.wilson@pharm.utah.edu>
2021-11-28 22:26:12 -0700

    changes

---
+++ index.js
@@ -0,0 +1,104 @@
+var PharmacyPhreaks = {
+    first_name: "Aaron",
+    last_name: "Wilson",
+    age: 30,
+    favorite_subjects: ["Anticoagulation", "Clinical Research", "Informatics", "Implementation Science"],
+    PharmPhriends: [
+        {
+            name: "Joe",
+            age: 30,
+            current_job: "Toxicology Fellow",
+            favorite_subjects: [
+                {
+                    subject_name: "Toxicology",
+                    subject_location: "Poison Control Center"
+                },
+                {
+                    subject_name: "Clinical Research",
+                    subject_location: "College of Pharmacy"
+                },
+                {
+                    subject_name: "Emergency Medicine",
+                    subject_location: "University of Utah Hospital"
+                }
+            ]
+        },
+        {
+            name: "Karlee",
+            age: 29,
+            current_job: "Intermountain Resident",
+            favorite_subjects: [
+                {
+                    subject_name: "Acute Care",
+                    subject_location: "Intermountain Hospital"
+                },
+                {
+                    subject_name: "Drug Information",
+                    subject_location: "College of Pharmacy"
+                },
+                {
+                    subject_name: "Emergency Medicine",
+                    subject_location: "University of Utah Hospital"
+                }
+            ]   
+        },
+        {
+            name: "Bryce",
+            age: 30,
+            current_job: "Peace Health Resident",
+            favorite_subjects: [
+                {
+                    subject_name: "Ambulatory Care",
+                    subject_location: "University of Utah Hospital"
+                },
+                {
+                    subject_name: "Drug Information",
+                    subject_location: "College of Pharmacy"
+                },
+                {
+                    subject_name: "Herbal Medicine",
+                    subject_location: "College of Pharmacy"
+                }
+            ]   
+        },
+        {
+            name: "Big Joe",
+            age: 29,
+            current_job: "Intermountain Resident",
+            favorite_subjects: [
+                {
+                    subject_name: "Ambulatory Care",
+                    subject_location: "Intermountain Hospital"
+                },
+                {
+                    subject_name: "Cardiology",
+                    subject_location: "University of Utah Hospital"
+                },
+                {
+                    subject_name: "Managed Care",
+                    subject_location: "College of Pharmacy"
+                }
+            ]   
+        },
+        {
+            name: "Sawyer",
+            age: 29,
+            current_job: "Pending",
+            favorite_subjects: [
+                {
+                    subject_name: "Informatics",
+                    subject_location: "University of Utah Hosptial"
+                },
+                {
+                    subject_name: "Clinical Research",
+                    subject_location: "College of Pharmacy"
+                },
+                {
+                    subject_name: "Toxicology",
+                    subject_location: "Poison Control Center"
+                }
+            ]   
+        }
+    ]
+}
+console.log("Hello! My name is " + PharmacyPhreaks.first_name + " " + PharmacyPhreaks.last_name +". It is a pleasure to meet you!")
